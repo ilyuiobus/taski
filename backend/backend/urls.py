4c543df635c6f3a1166739a665cffa52676134dc
ilyuiobus <Volinaiiv@yandex.ru>
2026-09-19 21:10:56 +0300

    Fix imports

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-
 from api import views
+
 
 router = routers.DefaultRouter()
 router.register('tasks', views.TaskView, 'task')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
